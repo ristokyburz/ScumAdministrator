621818bf5a7cd89136eac7390f04e865935ac891
ristokyburz <ristokyburz@AQUAMAN>
2016-08-25 11:09:44 +0200

    Use MUI

---
+++ ScrumAdministrator.Cockpit.Mod/StoryOverviewView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace ScrumAdministrator.Cockpit.Mod
+{
+    /// <summary>
+    /// Description for StoryOverviewView.
+    /// </summary>
+    public partial class StoryOverviewView : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the StoryOverviewView class.
+        /// </summary>
+        public StoryOverviewView()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- ScrumAdministrator.Cockpit/MainWindow.xaml.cs
+++ ScrumAdministrator.Cockpit/MainWindow.xaml.cs
@@ -1,12 +1,13 @@
 ﻿using System.Windows;
 using ScrumAdministrator.Cockpit.ViewModel;
+using FirstFloor.ModernUI.Windows.Controls;
 
 namespace ScrumAdministrator.Cockpit
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : ModernWindow
     {
         /// <summary>
         /// Initializes a new instance of the MainWindow class.
